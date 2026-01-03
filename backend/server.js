5d29dcc5d1c59cef308ac57e2e7e85e66e3c6222
idesta <idesta705@gmail.com>
2026-01-03 17:59:28 +0300

    Deploying with single path using the package json and server js

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 import { connectDB } from "./config/db.js";
 // import Product from "./models/product.model.js";
 // import { mongo } from "mongoose";
@@ -12,13 +13,24 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 app.use(express.json()); // allows us to accept JSON data in request body (req.b)
 
 app.use("/api/products", productRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  // app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.use(express.static(path.join(__dirname, "frontend", "dist")));
+
+  // Fallback route for SPA (catch all routes)
+
+  app.use((req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   connectDB();
   console.log("Server started at http://localhost:" + PORT);
 });
-
-//export default app;
